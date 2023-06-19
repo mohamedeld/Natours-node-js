4d700ca4bddaaf864f71c4b0903dca83c3499906
mohamedeld <mohamed.azoz20010@gmail.com>
2023-06-19 18:04:28 +0300

    connected to database

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 const app = require("./index");
 const dotEnv = require("dotenv");
 dotEnv.config({path:'./config.env'});
-const DB = process.env.DATABASE.replace("<password>",process.env.DATABASE_PASSWORD);
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:false
-}).then(()=> console.log("connection successful."));
-
-
 const PORT = process.env.PORT || 8000;
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    family:4
+}).then(()=> {
 
-app.listen(PORT, () => {
-    console.log("listening on port " + PORT);
-});
+    app.listen(PORT, () => {
+        console.log("listening on port " + PORT);
+    });
+    console.log("connection successful.")
+}).catch(err=> console.log(err));
+
+
+
 
